a38ad575de436a7e7a7953c514a925bf0751998c
26erika <develop.erika@gmail.com>
2020-08-06 20:52:29 -0500

    Added external links buttons styled also the URLs to opened in the browser

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -5,11 +5,44 @@
       alt="coming soon"
       src="../assets/proximamente.png"
     />
+    <ExternalLink
+      class="btn"
+      :external="{
+        url:
+          'https://docs.google.com/document/d/1zWYvQ3RUKzu1ohTfcbYYdzQZd-0_YhgStFIWUEGFrRY/edit?usp=sharing',
+        description: 'Sponsors'
+      }"
+      description="Sponsors"
+    />
+    <ExternalLink
+      class="btn"
+      :external="{
+        url: 'https://convoca.dev/jconf-mexico-2020/cfp',
+        description: 'Speakers'
+      }"
+      description="Speakers"
+    />
   </div>
 </template>
 
 <script>
+import ExternalLink from '../components/External_Link';
 export default {
-  name: 'Home'
+  name: 'Home',
+  components: { ExternalLink }
 };
 </script>
+
+<style lang="scss">
+.home > img {
+  width: 50%;
+}
+.btn > button {
+  background-color: #9b51e0;
+  color: white;
+  font-size: 24px;
+  padding: 10px;
+  border-color: #eb5757;
+  cursor: pointer;
+}
+</style>
