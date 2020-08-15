b4c5d1592dbd3ea1b5a5ecd2422e44e357c30525
Sinuhe Jaime <sierisimo@gmail.com>
2020-08-14 23:44:45 -0500

    Authentication components, finally
    This includes the tests and the AuthButton.
    There's still a future commit to include Firebase with ENV variables

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,23 @@
 <template>
   <div class="home">
+    <AuthButton
+      :model="googleProvider"
+      class="google"
+      v-on:value="nameUser = $event"
+    />
+    <AuthButton
+      :model="microsoftProvider"
+      class="microsoft"
+      v-on:value="nameUser = $event"
+    />
+    <AuthButton
+      :model="facebookProvider"
+      class="microsoft"
+      v-on:value="nameUser = $event"
+    />
+    <h1
+      v-text="nameUser ? 'Hola ' + this.nameUser : 'Aún no te has logeado :('"
+    ></h1>
     <img
       id="commingSoonImage"
       alt="coming soon"
@@ -10,14 +28,29 @@
   </div>
 </template>
 
-<script>
+<script lang="ts">
+import AuthButton from '@/components/AuthButton.vue';
+import Auth from '@/components/Auth.model';
+import firebase from 'firebase';
 import ExternalLinkButton from '../components/ExternalLinkButton';
 import external from '@/components/ExternalLink.model';
+
 export default {
   name: 'Home',
-  components: { ExternalLinkButton },
-
+  components: { ExternalLinkButton, AuthButton },
   computed: {
+    googleProvider: function() {
+      return new Auth(new firebase.auth.GoogleAuthProvider(), 'Google');
+    },
+    microsoftProvider: function() {
+      return new Auth(
+        new firebase.auth.OAuthProvider('microsoft'),
+        'Microsoft'
+      );
+    },
+    facebookProvider: function() {
+      return new Auth(new firebase.auth.FacebookAuthProvider(), 'Facebook');
+    },
     urlSponsor: function() {
       return this.createURL(
         'https://docs.google.com/document/d/1zWYvQ3RUKzu1ohTfcbYYdzQZd-0_YhgStFIWUEGFrRY/edit?usp=sharing',
@@ -35,6 +68,11 @@ export default {
     createURL: (url, description) => {
       return new external(url, description);
     }
+  },
+  data() {
+    return {
+      nameUser: ''
+    };
   }
 };
 </script>
@@ -43,6 +81,7 @@ export default {
 .home > img {
   width: 50%;
 }
+
 .btn > button {
   background-color: #9b51e0;
   color: white;
