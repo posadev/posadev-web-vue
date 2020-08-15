414a7fe88b613bc1600abfd75c2e97f8e8fbd45a
GeorginaPQ <giorginapqe@gmail.com>
2020-08-14 19:09:10 -0500

    :sparkles: Adding functionality to show a sponsorList with mockdata

---
+++ src/components/SponsorList.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <div v-for="item in items" v-bind:key="item.name">
+      <SponsorItem :item="item" />
+    </div>
+  </div>
+</template>
+
+<script>
+import SponsorItem from '@/components/SponsorItem';
+import MockList from '@/components/ms';
+
+export default {
+  name: 'SponsorList',
+  components: {
+    SponsorItem
+  },
+  data() {
+    return {
+      items: MockList
+    };
+  }
+};
+</script>
+<style>
+.home > img {
+  width: 50%;
+}
+</style>
