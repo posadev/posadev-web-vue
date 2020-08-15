3eb47ff793195963581286445c327bddca844a4f
GeorginaPQ <giorginapqe@gmail.com>
2020-08-14 19:24:08 -0500

    :construction: Work in progress at new component
    The component receives props and displays the images and name in the browser

---
+++ src/components/SponsorItem.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <p>{{ this.item.name }}</p>
+    <img :src="this.item.pic" />
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator';
+import Sponsor from '@/components/Sponsor.model';
+
+@Component
+export default class SponsorItem extends Vue {
+  @Prop() private item!: Sponsor;
+}
+</script>
