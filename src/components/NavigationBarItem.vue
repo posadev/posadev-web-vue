c3c975fda748d200b06486866a900e8b80d78c70
26erika <develop.erika@gmail.com>
2020-08-17 17:13:58 -0500

    Creation of the elements in the navigation bar also testing it

---
+++ src/components/NavigationBarItem.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <a @click="goToNavigationLink()"><slot></slot></a>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator';
+
+@Component
+export default class NavigationBarItem extends Vue {
+  @Prop() private path?: string;
+
+  private goToNavigationLink(): void {
+    //
+  }
+}
+</script>
