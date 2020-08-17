13aa380a4eee51b548bd00aee4b71b4252168655
unknown <eugynias>
2020-08-17 17:43:23 -0400

    Create TicketScreen and functionality with TicketCard

---
+++ src/views/TicketScreen.vue
@@ -0,0 +1,44 @@
+<template>
+  <div>
+    <div v-for="item in orderedItems" v-bind:key="item.name">
+      <TicketCardItem :item="item" />
+      <!-- <ExternalLinkButton :external="urlEvent" /> -->
+    </div>
+  </div>
+</template>
+<script>
+import TicketCardItem from '@/components/TicketCard';
+// import external from '@/components/ExternalLink.model';
+import mockTicket from '../components/mockTicket';
+// import ExternalLinkButton from '../components/ExternalLinkButton';
+
+export default {
+  name: 'TicketScreen',
+  components: {
+    TicketCardItem
+    //ExternalLinkButton
+  },
+  data() {
+    return {
+      items: mockTicket
+    };
+  },
+  computed: {
+    orderedItems() {
+      const orderTicket = this.items.slice().sort((a, b) => {
+        return a.price - b.price;
+      });
+      return orderTicket;
+    }
+  }
+  // urlEvent: function() {
+  //   return this.createURL('https://www.eventbrite.com.mx/', 'Comprar ahora');
+  // },
+
+  // methods: {
+  //   createURL: (url, description) => {
+  //     return new external(url, description);
+  //   }
+  // }
+};
+</script>
