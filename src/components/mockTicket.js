c591f74c0dc764a9d53ce415dc3cc8886d5cf72c
unknown <eugynias>
2020-08-17 16:57:56 -0400

    Create MockData for test functionality

---
+++ src/components/mockTicket.js
@@ -0,0 +1,23 @@
+export default [
+  {
+    name: 'Early Bird',
+    price: 2000,
+    start: '12 de Agosto 2020',
+    end: '13 de Agosto 2020',
+    url: 'https://www.eventbrite.com.mx/'
+  },
+  {
+    name: 'regular',
+    price: 1000,
+    start: '14 de Agosto 2020',
+    end: '135 de Agosto 2020',
+    url: 'https://www.eventbrite.com.mx/'
+  },
+  {
+    name: 'corperative',
+    price: 3000,
+    start: '16 de Agosto 2020',
+    end: '17 de Agosto 2020',
+    url: 'https://www.eventbrite.com.mx/'
+  }
+];
