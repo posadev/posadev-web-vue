0fe768d9222003c8d21514e8d171ba1d516825b4
unknown <eugynias>
2020-08-17 17:35:15 -0400

    Create Ticket Card Component

---
+++ src/components/TicketCard.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <div>
+      <span>{{ this.item.name }}</span>
+      <span>{{ this.item.price }}</span>
+      <span>{{ this.item.start }}</span>
+      <span>{{ this.item.end }}</span>
+    </div>
+    <div>
+      <!-- <ExternalLinkButton {{ this.item.url }} /> -->
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator';
+import ticketCard from '@/components/ticketCard.model';
+
+@Component
+export default class TicketCardItem extends Vue {
+  @Prop() private item!: ticketCard;
+}
+</script>
