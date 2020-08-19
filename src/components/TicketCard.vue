0886ff12f8100e2edac7c626b5328ad5e4dc6131
Sinuhe Jaime <sierisimo@gmail.com>
2020-08-19 12:28:07 -0500

    Revert "Create TicketCard components"
    This reverts commit 18f2d511c5c967d8ce51e7a317a8609686cb7b81.

---
+++ src/components/TicketCard.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <div>
+      <span>{{ this.item.name }}</span>
+      <span>{{ this.item.price }}</span>
+      <span>{{ this.item.start }}</span>
+      <span>{{ this.item.end }}</span>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator';
+import ticketCard from '@/components/ticketCard.model';
+
+@Component
+export default class TicketCardItem extends Vue {
+  @Prop() private item!: ticketCard;
+}
+</script>
