a0c63d60846f1f8d16186752c6bfef67b2451694
unknown <eugynias>
2020-08-20 14:05:42 -0400

    Add ActionButton and functionality

--- src/components/TicketCard.vue
+++ src/components/TicketCard.vue
@@ -4,18 +4,32 @@
       <span>{{ this.ticket.name }}</span>
       <span>{{ this.ticket.price }}</span>
       <span>{{ this.ticket.start }}</span>
-      <br />
       <span>{{ this.ticket.end }}</span>
     </div>
+    <ActionButton 
+    :info="ticketInfo"
+    v-on:button-action="goTicketLink"
+    />
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Prop, Vue } from 'vue-property-decorator';
 import Ticket from '@/data/Ticket.model';
+import ActionButton from '@/components/ActionButton.vue';
+import ButtonInfo from '@/data/ButtonInfo.model';
 
-@Component
+@Component({
+  components: { ActionButton }
+})
 export default class TicketCard extends Vue {
   @Prop() private ticket!: Ticket;
+  private ticketInfo = new ButtonInfo('Comprar ahora');
+
+  private goTicketLink() {
+    window.open(
+     this.ticket.url
+    );
+  }
 }
 </script>
