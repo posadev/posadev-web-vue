814adea2f2dc48effd99fb2944eb56330e45dbef
Sinuhe Jaime <sierisimo@gmail.com>
2020-08-20 21:48:01 -0500

    Adding pending test for Sponsors
    Currently using static mocks, not proud of it

--- tests/unit/components/SponsorItem.spec.ts
+++ tests/unit/components/SponsorItem.spec.ts
@@ -1,13 +1,13 @@
 import { shallowMount } from '@vue/test-utils';
-import sponsorItem from '@/components/SponsorItem.vue';
+import SponsorItem from '@/components/SponsorItem.vue';
 
 describe('SponsorItem.vue', () => {
   beforeEach(() => {
     window.open = jest.fn();
   });
   it('it should check if the img is clickable', () => {
-    const wrapper = shallowMount(sponsorItem, {
-      propsData: { sponsor: { url: '', description: '' } }
+    const wrapper = shallowMount(SponsorItem, {
+      propsData: {sponsor: {url: '', description: ''}}
     });
     wrapper.trigger('click');
     expect(window.open).toHaveBeenCalled();
