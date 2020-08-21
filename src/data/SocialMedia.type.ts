74c7ff2ba2264ba5d04db117293a9777b426dfed
Sinuhe Jaime <sierisimo@gmail.com>
2020-08-20 20:03:15 -0500

    Fixing style

--- src/data/SocialMedia.type.ts
+++ src/data/SocialMedia.type.ts
@@ -1,7 +1,7 @@
 export type SocialMedia = {
-    readonly twitter?: URL,
-    readonly facebook?: URL,
-    readonly instagram?: URL,
-    readonly github?: URL,
-    readonly linkedIn?: URL
-}
+  readonly twitter?: URL;
+  readonly facebook?: URL;
+  readonly instagram?: URL;
+  readonly github?: URL;
+  readonly linkedIn?: URL;
+};
