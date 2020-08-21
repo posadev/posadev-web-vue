a18ca8f6f6513c89897c34a67bffe4c527182c2c
Sinuhe Jaime <sierisimo@gmail.com>
2020-08-20 23:34:12 -0500

    Creating a section instead of the component

---
+++ src/components/Workshops.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="columns workshop-items">
+    <WorkshopCard
+      class="column col-lg-4 col-md-8 col-sm-10 col-xs-12 workshop-item"
+      v-for="workshop in workshops"
+      v-bind:info="workshop"
+      v-bind:key="workshop.name"
+    />
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+import WorkshopInfo from '@/data/WorkshopInfo.model';
+import ActionButton from '@/components/ActionButton.vue';
+import WorkshopCard from '@/components/WorkshopCard.vue';
+import workshopMocks from '@/mocks/Workshops.mock';
+
+@Component({
+  components: { ActionButton, WorkshopCard }
+})
+export default class Workshops extends Vue {
+  get workshops(): WorkshopInfo[] {
+    return workshopMocks;
+  }
+}
+</script>
+
+<style lang="scss">
+@import '../../node_modules/spectre.css/src/variables';
+
+@import '../styles/variables';
+@import '../../node_modules/spectre.css/src/layout';
+
+.section-workshops {
+  display: flex;
+  flex-direction: column;
+  background: $primary-color;
+}
+
+.workshop-items {
+  justify-content: center;
+}
+
+.workshop-item {
+  margin: {
+    top: 1%;
+    bottom: 1%;
+  }
+}
+
+.btn-see-all {
+  align-self: center;
+}
+</style>
---
+++ src/mocks/Workshops.mock.ts
@@ -0,0 +1,14 @@
+import WorkshopInfo from '@/data/WorkshopInfo.model';
+
+const photoUrl = new URL('https://via.placeholder.com/126x126');
+const workshops = [
+  new WorkshopInfo('Memes para todos1', 'El Joel', photoUrl),
+  new WorkshopInfo('Memes para todos2', 'El Joel', photoUrl),
+  new WorkshopInfo('Memes para todos3', 'El Joel', photoUrl),
+  new WorkshopInfo('Memes para todos4', 'El Joel', photoUrl),
+  new WorkshopInfo('Memes para todos5', 'El Joel', photoUrl),
+  new WorkshopInfo('Memes para todos5', 'El Joel', photoUrl),
+  new WorkshopInfo('Memes para todos6', 'El Joel', photoUrl)
+];
+
+export default workshops;
