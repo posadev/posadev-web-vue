68b132d5832fc335f66f202d8d7bc8a8fc161c51
Sinuhe Jaime <sierisimo@gmail.com>
2020-08-21 21:51:01 -0500

    Replacing css with SCSS and repeated components with v-for

--- src/components/NavigationBar.vue
+++ src/components/NavigationBar.vue
@@ -6,27 +6,17 @@
     <label for="toggle">&#9776;</label>
     <input type="checkbox" id="toggle" />
     <div class="navBarMenuContainer">
-      <NavigationBarItem class="navBarItem" :path="'/presentadores'"
-        >Presentadores</NavigationBarItem
+      <NavigationBarItem
+        v-for="barItem in items"
+        class="navBarItem"
+        v-bind:key="barItem.path"
+        :path="barItem.path"
       >
-      <NavigationBarItem class="navBarItem" :path="'/agenda'"
-        >Agenda</NavigationBarItem
-      >
-      <NavigationBarItem class="navBarItem" :path="'/sede'"
-        >Sede</NavigationBarItem
-      >
-      <NavigationBarItem class="navBarItem" :path="'/patrocinadores'"
-        >Patrocinadores</NavigationBarItem
-      >
-      <NavigationBarItem class="navBarItem" :path="'/login'"
-        >Login</NavigationBarItem
-      >
-      <NavigationBarItem class="navBarItem" :path="'/idioma'"
-        >Idioma</NavigationBarItem
-      >
-      <NavigationBarItem class="navBarItem navBarTicket" :path="'/boletos'"
-        >Boletos</NavigationBarItem
-      >
+        {{ barItem.text }}
+      </NavigationBarItem>
+      <NavigationBarItem class="navBarItem navBarTicket" :path="'/tickets'">
+        Boletos
+      </NavigationBarItem>
     </div>
   </nav>
 </template>
@@ -34,112 +24,52 @@
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
 import NavigationBarItem from './NavigationBarItem.vue';
+import { BarItem, navigationItems } from '@/data/BarItem.type';
 
 @Component({
   components: { NavigationBarItem }
 })
-export default class NavigationBar extends Vue {}
+export default class NavigationBar extends Vue {
+  private items: BarItem[] = navigationItems;
+}
 </script>
 
 <style lang="scss">
 @import '../styles/variables';
+
+@mixin visible-scaleX($scale-value: 1) {
+  visibility: visible;
+  transform: scaleX($scale-value);
+}
+
+@mixin query-only-screen-max-width($value-max-width: 768px) {
+  @media only screen and (max-width: $value-max-width) {
+    @content;
+  }
+}
+
+%visible-scale1-media {
+  @include visible-scaleX(1);
+
+  @media only screen and (max-width: 768px) {
+    @include visible-scaleX(1);
+  }
+}
+
 .navBar {
   display: flex;
+  position: sticky;
   background-color: $primary-dark-color;
-  flex-direction: row;
-  flex-wrap: wrap;
+  flex: {
+    direction: row;
+    wrap: wrap;
+  }
   align-items: center;
   justify-content: space-between;
   height: 139px;
   width: 100%;
-  position: sticky;
-}
-.navBarLogo {
-  max-height: 70px;
-  padding: 0 0 0 24px;
-  cursor: pointer;
-}
-.navBarMenuContainer {
-  width: 60%;
-  display: flex;
-  flex-direction: row;
-  flex-wrap: wrap;
-  align-items: center;
-  justify-content: space-around;
-}
-label {
-  margin: 0 40px 0 0;
-  font-size: 26px;
-  line-height: 70px;
-  display: none;
-  width: 26px;
-  float: right;
-  color: $inactive-color;
-}
-#toggle {
-  display: none;
-}
-.navBarItem {
-  position: relative;
-  font-size: 1rem;
-  color: #ffffff;
-  cursor: pointer;
-  font: {
-    family: $project-font;
-    size: 16px;
-  }
-  clear: right;
-}
-.navBarItem ::before {
-  content: '';
-  position: absolute;
-  width: 100%;
-  height: 3px;
-  bottom: -3px;
-  left: 0;
-  background-color: $accent-color;
-  visibility: hidden;
-  border-radius: 5px;
-  transform: scaleX(0);
-  transition: 0.25s linear;
-}
-.navBarItem :hover:before,
-.navBarItem :active::before {
-  visibility: visible;
-  transform: scaleX(1);
-}
-.navBarTicket {
-  background-color: $accent-color;
-  padding: calc(120px / 2) 25px calc(120px / 2) 25px;
-  color: $primary-dark-color;
-  cursor: pointer;
-  font: {
-    family: $project-font;
-    size: 16px;
-    weight: 800;
-  }
-}
-.navBarTicket ::before {
-  content: '';
-  position: absolute;
-  width: 55px;
-  height: 3px;
-  bottom: calc(119px / 2);
-  left: auto;
-  right: auto;
-  background-color: $primary-dark-color;
-  visibility: hidden;
-  border-radius: 5px;
-  transform: scaleX(0);
-  transition: 0.25s linear;
-}
-.navBarTicket :hover:before,
-.navBarTicket :active::before {
-  visibility: visible;
-  transform: scaleX(1);
-}
-@media only screen and (max-width: 768px) {
-  .navBar {
+
+  @include query-only-screen-max-width {
     position: sticky;
     display: flex;
     background-color: $primary-dark-color;
@@ -148,63 +78,157 @@ label {
     height: 100%;
     width: 100%;
   }
-  .navBarMenuContainer {
-    background-color: $primary-dark-color;
-    height: auto;
+}
+
+.navBarLogo {
+  max-height: 70px;
+  padding: 0 0 0 24px;
+  cursor: pointer;
+}
+
+.navBarMenuContainer {
+  width: 60%;
+  display: flex;
+  flex: {
+    direction: row;
+    wrap: wrap;
   }
-  .navBarItem {
-    padding: 15px 0 15px 0;
+  align-items: center;
+  justify-content: space-around;
+}
+
+label {
+  display: none;
+  width: 26px;
+  float: right;
+  margin: 0 40px 0 0;
+  font-size: 26px;
+  line-height: 70px;
+  color: $inactive-color;
+
+  @include query-only-screen-max-width {
+    display: block;
+    cursor: pointer;
   }
-  .navBarItem ::before {
+}
+
+#toggle {
+  display: none;
+
+  &:checked {
+    + .navBarMenuContainer {
+      @include query-only-screen-max-width {
+        display: block;
+      }
+    }
+  }
+}
+
+.navBarItem {
+  position: relative;
+  color: #ffffff;
+  cursor: pointer;
+  font: {
+    family: $project-font;
+    size: 1rem;
+  }
+  clear: right;
+
+  %nav-item-hide {
     content: '';
     position: absolute;
-    width: 100%;
     height: 3px;
-    bottom: 7px;
-    left: 0;
+    width: 100%;
     background-color: $accent-color;
     visibility: hidden;
     border-radius: 5px;
     transform: scaleX(0);
     transition: 0.25s linear;
+    left: 0;
   }
-  .navBarItem :hover:before,
-  .navBarItem :active::before {
-    visibility: visible;
-    transform: scaleX(1);
+
+  ::before {
+    @extend %nav-item-hide;
+    bottom: -3px;
+    border-radius: 5px;
+
+    @include query-only-screen-max-width {
+      @extend %nav-item-hide;
+      bottom: 7px;
+    }
   }
-  .navBarTicket ::before {
+
+  :hover:before {
+    @extend %visible-scale1-media;
+  }
+
+  :active::before {
+    @extend %visible-scale1-media;
+  }
+
+  @include query-only-screen-max-width {
+    padding: 15px 0 15px 0;
+  }
+}
+
+.navBarTicket {
+  background-color: $accent-color;
+  padding: calc(120px / 2) 25px calc(120px / 2) 25px;
+  color: $primary-dark-color;
+  cursor: pointer;
+  font: {
+    family: $project-font;
+    size: 1rem;
+    weight: 800;
+  }
+
+  %nav-ticket-hide {
     content: '';
     position: absolute;
-    width: 100%;
     height: 3px;
     bottom: calc(100% / 4);
-    background-color: $primary-dark-color;
     visibility: hidden;
+    background-color: $primary-dark-color;
     border-radius: 5px;
     transform: scaleX(0);
     transition: 0.25s linear;
   }
-  .navBarTicket :hover:before,
-  .navBarTicket :active::before {
-    visibility: visible;
-    transform: scaleX(1);
+
+  ::before {
+    @extend %nav-ticket-hide;
+    width: 55px;
+    left: auto;
+    right: auto;
+
+    @include query-only-screen-max-width {
+      @extend %nav-ticket-hide;
+      width: 100%;
+    }
   }
-  label {
-    display: block;
-    cursor: pointer;
+
+  :hover:before {
+    @extend %visible-scale1-media;
   }
-  .navBarMenuContainer {
-    text-align: center;
-    width: 100%;
+
+  :active::before {
+    @extend %visible-scale1-media;
+  }
+}
+
+.navBarMenuContainer {
+  @include query-only-screen-max-width {
     display: none;
+    height: auto;
+    width: 100%;
+    background-color: $primary-dark-color;
+    text-align: center;
   }
-  .navBarMenuContainer a {
-    display: block;
-    margin: 0;
-  }
-  #toggle:checked + .navBarMenuContainer {
-    display: block;
+
+  a {
+    @include query-only-screen-max-width {
+      display: block;
+      margin: 0;
+    }
   }
 }
 </style>
