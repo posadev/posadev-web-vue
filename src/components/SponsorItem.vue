513dda47b8cc13d5d3445022eaede530943ac03f
GeorginaPQ <giorginapqe@gmail.com>
2020-08-24 18:24:31 -0500

    Update styles using media querys

--- src/components/SponsorItem.vue
+++ src/components/SponsorItem.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="sponsor" @click="goToLink">
-    <img :src="this.sponsor.pic" :alt="this.sponsor.name" />
+    <img class="photosponsor" :src="this.sponsor.pic" :alt="this.sponsor.name" />
   </div>
 </template>
 
@@ -17,4 +17,31 @@ export default class SponsorItem extends Vue {
   }
 }
 </script>
-<style lang="scss"></style>
+<style lang="scss">
+
+@mixin query-only-screen-max-width($value-max-width: 1024px) {
+  @media only screen and (max-width: $value-max-width) {
+    @content;
+  }
+}
+.sponsor{
+  @include query-only-screen-max-width {
+    width: 138px;
+    height: 51px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    background: black;
+  }
+  width:196px;
+  height: 84px;
+  .photosponsor{
+    @include query-only-screen-max-width {
+      width: 136.71px;
+      height: 50.15px;
+    }
+    width: 196px;
+    height: 84px;
+  }
+}
+</style>
