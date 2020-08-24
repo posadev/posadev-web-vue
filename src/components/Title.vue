429be4f6bb560437aab1cea5f1e93177211e6eb6
Sinuhe Jaime <sierisimo@gmail.com>
2020-08-23 21:21:31 -0500

    Simplest component for all shared titles-subtitles
    This is present in most of the sections and can be customized passing own classes for alignment or color

---
+++ src/components/Title.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <h1 class="title">{{ this.texts.title }}</h1>
+    <p v-if="this.texts.subtitle">{{ this.texts.subtitle }}</p>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator';
+import TitleTexts from '@/data/TitleTexts.model';
+
+@Component
+export default class Title extends Vue {
+  @Prop({ required: true }) private texts!: TitleTexts;
+}
+</script>
+
+<style lang="scss">
+@import '../styles/variables';
+
+.title {
+  font: {
+    style: normal;
+    weight: bold;
+    size: 55px;
+  }
+  line-height: 30px;
+}
+
+.subtitle {
+  font: {
+    style: normal;
+    weight: normal;
+    size: 24px;
+  }
+  line-height: 45px;
+}
+</style>
---
+++ src/data/TitleTexts.model.ts
@@ -0,0 +1,4 @@
+export default class TitleTexts {
+  constructor(readonly title: string, readonly subtitle: string | null = null) {
+  }
+}
