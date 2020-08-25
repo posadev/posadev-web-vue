d4637dfa79b9d5cd71364cee914269f125c5b36f
GeorginaPQ <giorginapqe@gmail.com>
2020-08-25 11:00:47 -0500

    Update styles for centered columns

--- src/components/Sponsors.vue
+++ src/components/Sponsors.vue
@@ -1,13 +1,12 @@
 <template>
-  <div class="container">
     <div class="columns">
       <SponsorItem
+        class="column col-2 col-md-3 col-xs-5"
         v-for="sponsor in sponsors"
         v-bind:key="sponsor.name"
         :sponsor="sponsor"
       />
     </div>
-  </div>
 </template>
 
 <script lang="ts">
@@ -29,4 +28,10 @@ export default class SponsorList extends Vue {
 <style lang="scss">
 @import '../styles/variables';
 @import '~spectre.css/src/_layout';
+.columns {
+  padding: 0;
+  margin: 0;
+  align-items: center;
+  justify-content: center;
+}
 </style>
