a9382711b784224bdeed6e1999b5acc90fd49538
unknown <eugynias>
2020-08-26 18:13:48 -0400

    Adding styles for the ticket

--- src/components/TicketCard.vue
+++ src/components/TicketCard.vue
@@ -1,12 +1,18 @@
 <template>
-  <div>
-    <div class="ticket">
-      <h2>{{ this.ticket.name }}</h2>
-      <h1>{{ this.ticket.price }}</h1>
-      <p>{{ formatDate(this.ticket.start) }} - {{ formatDate(this.ticket.end)  }}</p>
-      <TicketButton :info="ticketInfo" 
-      v-on:button-action="goTicketLink" />
+  <div class="card text-center">
+    <div class="card-header ">
+      <p class="name">{{ this.ticket.name }}</p>
+      <p class="price">{{ this.ticket.price }}</p>
+      <p class="date">
+        {{ formatDate(this.ticket.start) }}
+        - {{ formatDate(this.ticket.end) }}
+      </p>
     </div>
+    <TicketButton
+      class="card-footer center"
+      :info="ticketInfo"
+      v-on:button-action="goTicketLink"
+    />
   </div>
 </template>
 
@@ -27,7 +33,7 @@ export default class TicketCard extends Vue {
     window.open(this.ticket.url.toString());
   }
   private formatDate(date: Date) {
-   const months = [
+    const months = [
       'Ene',
       'Feb',
       'Mar',
@@ -41,25 +47,46 @@ export default class TicketCard extends Vue {
       'Nov',
       'Dic'
     ];
-    return`${date.getDate()}/${months[date.getMonth()]}/${date.getFullYear() -2000}`;
+    return `${date.getDate()}/${months[date.getMonth()]}/${date.getFullYear() -
+      2000}`;
   }
-  
 }
 </script>
 <style lang="scss">
 @import '../../node_modules/spectre.css/src/variables';
 @import '../../node_modules/spectre.css/src/layout';
+@import '../../node_modules/spectre.css/src/hero';
 @import '../styles/_variables.scss';
 
-.ticket {
+.card {
   width: 306px;
   height: 316px;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  background: $inactive-color;
-  
+}
+.card-footer {
   margin: 0;
+  padding: 0;
+}
+
+.name {
   font-family: $project-font;
+  font-style: normal;
+  font-weight: bold;
+  font-size: 24px;
+  margin-top: 6px;
+}
+.price {
+  font-family: Roboto;
+  font-style: normal;
+  font-weight: bold;
+  font-size: 55px;
+  margin-top: 0;
+  margin-bottom: 20px;
+  color: $inactive-color;
+}
+.date {
+  font-family: Roboto;
+  font-style: normal;
+  font-weight: normal;
+  font-size: 16px;
 }
 </style>
