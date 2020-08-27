2361c68eb5ca04db85a10807925f2e1325d8af22
GeorginaPQ <giorginapqe@gmail.com>
2020-08-27 01:53:41 -0500

    The new component receives and displays the image and info in the browser

---
+++ src/components/SpeakerCard.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <div>
+      <img :src="this.speaker.photoURL" :alt="this.speaker.company" />
+      <p>{{ this.speaker.company }}</p>
+    </div>
+    <div>
+      <p>{{ this.speaker.speakerName }}</p>
+      <p>{{ this.speaker.bio }}</p>
+    </div>
+  </div>
+</template>
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator';
+import Speaker from '../data/Speaker.model';
+
+@Component
+export default class SpeakerCard extends Vue {
+  @Prop({ required: true }) private speaker!: Speaker;
+}
+</script>
+<style lang="scss"></style>
