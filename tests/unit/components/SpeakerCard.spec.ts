56e71124111b5ebad323263095d81ab20e807be0
GeorginaPQ <giorginapqe@gmail.com>
2020-08-27 11:53:54 -0500

    remove linter warnings

--- tests/unit/components/SpeakerCard.spec.ts
+++ tests/unit/components/SpeakerCard.spec.ts
@@ -4,22 +4,21 @@ import Speaker from '@/data/Speaker.model';
 
 describe('SpeakerCard component', () => {
   const fake: Speaker = new Speaker(
-    'Java Developer1', 
-    'Mark Welch', 
-    new URL ('https://via.placeholder.com/306x236'), 
+    'Java Developer1',
+    'Mark Welch',
+    new URL('https://via.placeholder.com/306x236'),
     'IBM'
-  )
+  );
   it('should render properly', () => {
     const wrapper = shallowMount(SpeakerCard, {
-      propsData: { 
+      propsData: {
         speaker: fake
-    }
+      }
     });
     const photo = wrapper.find('img.photo-speaker');
     const company = wrapper.find('p.company');
     const name = wrapper.find('p.name');
     const ocupation = wrapper.find('p.bio');
-    
     expect(photo.exists()).toBe(true);
     expect(photo.attributes()['src']).toBe(fake.photoURL.toString());
     expect(company.text()).toBe(fake.company);
