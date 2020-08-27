7714514ec3e7f84778ceadb2d041f7eab3478f0f
GeorginaPQ <giorginapqe@gmail.com>
2020-08-27 12:20:11 -0500

    remove linter warnings

--- tests/unit/components/Speakers.spec.ts
+++ tests/unit/components/Speakers.spec.ts
@@ -1,4 +1,4 @@
-import { mount, shallowMount } from '@vue/test-utils';
+import { shallowMount } from '@vue/test-utils';
 import Speakers from '@/components/Speakers.vue';
 import SpeakerCard from '@/components/SpeakerCard.vue';
 
