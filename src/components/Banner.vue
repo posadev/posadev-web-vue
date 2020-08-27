aa72b7be8d80995a03fb447468077928f4f873cb
Yaidi <yaidi.garcia.lopez@gmail.com>
2020-08-27 12:34:17 -0500

    Add banner in home view and component banner with image

---
+++ src/components/Banner.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="col-xl-12 content">
+    <img
+      src="https://firebasestorage.googleapis.com/v0/b/jconfmex-web-staging.appspot.com/o/banner.png?alt=media&token=592557de-1352-4d6f-8adf-ee149dbe3953"
+      class="img-responsive bg"
+    />
+    <section class="textcontent">
+      <h1 class="title">J Conf GDL 2020</h1>
+      <p>El evento de Java más importante de México</p>
+    </section>
+  </div>
+</template>
+
+<script lang="ts">
+import { Vue, Component } from 'vue-property-decorator';
+
+@Component
+export default class Banner extends Vue {}
+</script>
+
+<style lang="scss">
+@import '../styles/variables';
+@import '~spectre.css/src/_layout';
+@import '~spectre.css/src/_media.scss';
+
+.content {
+  position: relative;
+  display: block;
+}
+.bg {
+  min-width: 100%;
+}
+.textcontent {
+  position: absolute;
+  display: block;
+  left: 6.67%;
+  right: 56.67%;
+  top: 4.42%;
+  bottom: 92.83%;
+  color: $inactive-color;
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,21 +1,26 @@
 <template>
-  <div class="home">
-    <img
-      id="commingSoonImage"
-      alt="coming soon"
-      src="../assets/proximamente.png"
-    />
-    <div class="external-buttons">
-      <AccentActionButton
-        id="btn-cfs"
-        :info="sponsorInfo"
-        v-on:button-action="onSponsorClick"
+  <div>
+    <div >
+      <Banner />
+    </div>
+    <div class="home">
+      <img
+        id="commingSoonImage"
+        alt="coming soon"
+        src="../assets/proximamente.png"
       />
-      <AccentActionButton
-        id="btn-cfp"
-        :info="speakerInfo"
-        v-on:button-action="onSpeakerClick"
-      />
+      <div class="external-buttons">
+        <AccentActionButton
+          id="btn-cfs"
+          :info="sponsorInfo"
+          v-on:button-action="onSponsorClick"
+        />
+        <AccentActionButton
+          id="btn-cfp"
+          :info="speakerInfo"
+          v-on:button-action="onSpeakerClick"
+        />
+      </div>
     </div>
   </div>
 </template>
@@ -25,9 +30,10 @@ import { Component, Vue } from 'vue-property-decorator';
 import ButtonInfo from '@/data/ButtonInfo.model';
 import ActionButton from '@/components/ActionButton.vue';
 import AccentActionButton from '@/components/AccentActionButton.vue';
+import Banner from '@/components/Banner.vue';
 
 @Component({
-  components: { AccentActionButton, ActionButton }
+  components: { AccentActionButton, ActionButton, Banner }
 })
 export default class Home extends Vue {
   private sponsorInfo = new ButtonInfo('Patrocina este evento');
