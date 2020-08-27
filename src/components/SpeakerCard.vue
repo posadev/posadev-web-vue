b970f3508965c2cfb8ddd9b2162ea6f9145c3711
GeorginaPQ <giorginapqe@gmail.com>
2020-08-27 11:53:30 -0500

    remove linter warnings

--- src/components/SpeakerCard.vue
+++ src/components/SpeakerCard.vue
@@ -1,12 +1,16 @@
 <template>
   <div>
     <div>
-      <img class="photo-speaker" :src="this.speaker.photoURL" :alt="this.speaker.company" />
-      <p class="company" >{{ this.speaker.company }}</p>
+      <img
+        class="photo-speaker"
+        :src="this.speaker.photoURL"
+        :alt="this.speaker.company"
+      />
+      <p class="company">{{ this.speaker.company }}</p>
     </div>
     <div>
-      <p class="name" >{{ this.speaker.speakerName }}</p>
-      <p class="bio" >{{ this.speaker.bio }}</p>
+      <p class="name">{{ this.speaker.speakerName }}</p>
+      <p class="bio">{{ this.speaker.bio }}</p>
     </div>
   </div>
 </template>
