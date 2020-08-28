9ed7f8f01f6a7b0225a77c7d92b6a617a6192501
Yaidi <yaidi.garcia.lopez@gmail.com>
2020-08-27 19:11:21 -0500

    Funciton

--- src/mocks/fetchImage.mock.ts
+++ src/mocks/fetchImage.mock.ts
@@ -4,19 +4,12 @@ import Storage = firebase.storage.Storage;
 
 const getDownloadURL = jest
     .fn()
-    .mockReturnValue(await URL);
-    
+    .mockReturnValue('gs://jconfmex-web-staging.appspot.com/about-event.jpg');
+
 const storageSpy = jest
     .spyOn(firebase.initializeApp({}), 'storage')
     .mockReturnValue(({ getDownloadURL } as unknown) as Storage);
 
-//const getDownloadURL = async() => URL;
-
-export async function fetchImage(imageName: string): Promise<string> {
-    return storageSpy
-};
-
-
 const mock = {
     'about-event.jpg': 'gs://jconfmex-web-staging.appspot.com/about-event.jpg'
-}
+};
--- src/service/fetchImage.ts
+++ src/service/fetchImage.ts
@@ -1,10 +1,9 @@
 import firebase from 'firebase/app';
 import 'firebase/storage';
 
-
-export async function fetchImage(imageName: string) :Promise<string> {
-    const storageRef: firebase.storage.Reference = firebase.storage().ref(imageName);
-    return storageRef
-      .getDownloadURL();
-       
-  }
+export function fetchImage(imageName: string): Promise<string> {
+  const storageRef: firebase.storage.Reference = firebase
+    .storage()
+    .ref(imageName);
+  return storageRef.getDownloadURL();
+}
