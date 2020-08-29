ea02c113f751ba623f0c9427b388d2933dc9ff51
Sinuhe Jaime <sierisimo@gmail.com>
2020-08-29 04:38:41 -0500

    Adding the section for Sponsors that will be present in home

---
+++ src/components/SectionSponsors.vue
@@ -0,0 +1,52 @@
+<template>
+  <SectionHero class="section-home section-white" :title="sponsorsTexts">
+    <template v-slot:hero-header>
+      <AccentActionButton
+        id="btn-cfs"
+        class="section-button"
+        :info="sponsorsInfo"
+        v-on:button-action="onCFSClick"
+      />
+    </template>
+    <template v-slot:hero-body>
+      <Sponsors />
+    </template>
+  </SectionHero>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+import TitleTexts from '@/data/TitleTexts.model';
+import ButtonInfo from '@/data/ButtonInfo.model';
+import SectionHero from '@/components/SectionHero.vue';
+import AccentActionButton from '@/components/AccentActionButton.vue';
+import Sponsors from '@/components/Sponsors.vue';
+
+@Component({
+  components: {
+    SectionHero,
+    AccentActionButton,
+    Sponsors
+  }
+})
+export default class SectionSponsors extends Vue {
+  private get sponsorsTexts(): TitleTexts {
+    return new TitleTexts(
+      this.$t('home.sponsors.title'),
+      this.$t('home.sponsors.subtitle')
+    );
+  }
+
+  private get sponsorsInfo(): ButtonInfo {
+    return new ButtonInfo(this.$t('home.sponsors.topButton'));
+  }
+
+  private onCFSClick() {
+    window.open(
+      'https://docs.google.com/document/d/1zWYvQ3RUKzu1ohTfcbYYdzQZd-0_YhgStFIWUEGFrRY/edit?usp=sharing'
+    );
+  }
+}
+</script>
+
+<style lang="scss"></style>
