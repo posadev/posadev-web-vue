6885532f293c4e8acf468f2c2b66a7ced21330ee
Sinuhe Jaime <sierisimo@gmail.com>
2020-08-29 00:04:15 -0500

    Updating versions of libs, also organizing locales

--- .eslintrc.js
+++ .eslintrc.js
@@ -14,7 +14,15 @@ module.exports = {
   ],
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'comma-dangle': [
+      'error',
+      {
+        imports: 'never',
+        exports: 'never',
+        functions: 'never'
+      }
+    ]
   },
   overrides: [
     {
