ddbeb852480b68d5e02112592b7d9b62e7b1a06f
unknown <eugynias>
2020-08-30 13:21:57 -0400

    Separated header on the other component

--- src/components/TicketCard.vue
+++ src/components/TicketCard.vue
@@ -1,16 +1,9 @@
 <template>
-  <div class="card center">
-    <div class="card-header center">
-      <p class="name">{{ this.ticket.name }}</p>
-      <p class="price">{{ this.ticket.price }}</p>
-      <p class="date">
-        {{ formatDate(this.ticket.start) }}
-        - {{ formatDate(this.ticket.end) }}
-      </p>
-    </div>
+  <div class="card">
+    <TicketHeader class="card-header" :ticket="ticket" />
     <TicketButton
-      class="card-footer center"
-      :info="ticketInfo"
+      class="card-footer"
+      :info="buttonInfo"
       :ticketUrl="ticket"
     />
   </div>
@@ -26,33 +19,12 @@ import ButtonInfo from '@/data/ButtonInfo.model';
   components: { TicketButton }
 })
 export default class TicketCard extends Vue {
-  @Prop() private ticket!: Ticket;
-  private ticketInfo = new ButtonInfo('Comprar ahora');
+  @Prop({required: true}) private ticket!: Ticket;
+  private buttonInfo = new ButtonInfo('Comprar ahora');
 
-  private formatDate(date: Date) {
-    const months = [
-      'Ene',
-      'Feb',
-      'Mar',
-      'Abr',
-      'May',
-      'Jun',
-      'Jul',
-      'Ago',
-      'Sep',
-      'Oct',
-      'Nov',
-      'Dic'
-    ];
-    return `${date.getDate()}/${months[date.getMonth()]}/${date.getFullYear() -
-      2000}`;
-  }
 }
 </script>
 <style lang="scss">
-@import '~spectre.css/src/_variables';
-@import '~spectre.css/src/_layout';
-@import '~spectre.css/src/_hero';
 @import '../styles/_variables.scss';
 
 .card {
@@ -62,51 +34,18 @@ export default class TicketCard extends Vue {
     color: $primary-color;
   }
 }
+
+.card-header {
+  background-color: white;
+  padding-top: 1rem;
+  padding-bottom: 1rem;
+}
+
 .card-footer {
   &:hover {
     background-color: $primary-color;
     color: $background-ticket;
     border: none;
   }
-}
-.card-header {
-  background-color: $background-ticket;
-  padding-top: 1rem;
-  padding-bottom: 1rem;
-}
-
-.name {
-  font: {
-    weight: bold;
-    size: 24px;
-  }
-  padding: {
-    top: 1.5rem;
-    bottom: 1rem;
-  }
-  color: $inactive-text;
-}
-
-.price {
-  font: {
-    weight: bold;
-    size: 55px;
-  }
-  padding: {
-    top: 1rem;
-    bottom: 2rem;
-  }
-  margin: {
-    top: 0;
-  }
-  color: black;
-}
-.date {
-  font: {
-    weight: normal;
-    size: 16px;
-  }
-  margin: 0;
-  color: rgba(0, 0, 0, 0.56);
 }
 </style>
