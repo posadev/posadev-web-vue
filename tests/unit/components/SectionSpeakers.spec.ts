b09b022c24faf8d01eaccaea51c742b1ab95eb85
Sinuhe Jaime <sierisimo@gmail.com>
2020-08-31 02:55:46 -0500

    Mocks for sectionSpeakers in tests

--- tests/unit/components/SectionSpeakers.spec.ts
+++ tests/unit/components/SectionSpeakers.spec.ts
@@ -2,6 +2,9 @@ import { mount, shallowMount } from '@vue/test-utils';
 import SectionSpeakers from '@/components/SectionSpeakers.vue';
 import AccentActionButton from '@/components/AccentActionButton.vue';
 import Speakers from '@/components/Speakers.vue';
+import Speaker from '@/data/Speaker.model';
+import speakers from '@/mocks/Speakers.mock';
+import flushPromises from 'flush-promises';
 
 describe('SectionSpeakers component', () => {
   beforeEach(() => {
@@ -10,6 +13,9 @@ describe('SectionSpeakers component', () => {
 
   it('should contain all subcomponents', () => {
     const wrapper = shallowMount(SectionSpeakers, {
+      provide: {
+        speakers: jest.fn().mockReturnValue({})
+      },
       mocks: {
         $t: jest.fn().mockReturnValue({})
       }
@@ -19,12 +25,21 @@ describe('SectionSpeakers component', () => {
     expect(wrapper.findComponent(Speakers).exists()).toBe(true);
   });
 
-  it('should call a method when a click happens', () => {
+  it('should call a method when a click happens', async () => {
     const wrapper = mount(SectionSpeakers, {
+      provide: {
+        speakers: {
+          async findAll(): Promise<Speaker[]> {
+            return new Promise((resolve) => resolve(speakers));
+          }
+        }
+      },
       mocks: {
         $t: jest.fn().mockReturnValue({})
       }
     });
+
+    await flushPromises();
 
     wrapper.find('#btn-cfp').trigger('click');
     expect(window.open).toHaveBeenCalled();
