55955fb7b918bb0b63d31b1b54eaf8354b53a74e
26erika <develop.erika@gmail.com>
2020-08-31 17:40:48 -0500

    Fixed firebase variables

--- src/firebase.ts
+++ src/firebase.ts
@@ -2,12 +2,12 @@ import * as firebase from 'firebase/app';
 import 'firebase/firestore';
 
 const firebaseApp = firebase.initializeApp({
-  apiKey: process.env.VUE_APP_FIREBASE_API_KEY,
+  apiKey: process.env.VUE_APP_FIREBASE_API,
   authDomain: process.env.VUE_APP_FIREBASE_AUTH_DOMAIN,
   databaseURL: process.env.VUE_APP_FIREBASE_DB_URL,
-  projectId: process.env.VUE_APP_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.VUE_APP_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.VUE_APP_FIREBASE_MESSAGING_SENDER_ID
+  projectId: process.env.VUE_APP_FIREBASE_PROJECT,
+  storageBucket: process.env.VUE_APP_FIREBASE_STORAGE,
+  messagingSenderId: process.env.VUE_APP_FIREBASE_MESSAGING_SENDER
 });
 
 const db = firebaseApp.firestore();
