32dcd55db280d40fc0480ab1f88c2c1454344222
Yaidi <yaidi.garcia.lopez@gmail.com>
2020-09-01 15:24:48 -0500

    About the event style and responsive, testing file

--- src/components/AboutEvent.vue
+++ src/components/AboutEvent.vue
@@ -1,10 +1,18 @@
 <template>
-  <div class="">
-    <img class="img-responsive" :src="image" alt="" />
-    <div class="columns">
-      <h2>{{ $t('aboutevent.title') }}</h2>
-      <h4>{{ $t('aboutevent.subtitle') }}</h4>
-      <p>{{ $t('aboutevent.description') }}</p>
+  <div class="column section-light section-home">
+    <div class="hero hero-lg content">
+      <div class="container-image">
+        <img
+          class="img-fit-cover image-about-event"
+          :src="image"
+          alt="about event"
+        />
+      </div>
+      <section class="text-content">
+        <h1 class="title">{{ $t('home.aboutevent.title') }}</h1>
+        <h4 class="subtitle">{{ $t('home.aboutevent.subtitle') }}</h4>
+        <p class="text">{{ $t('home.aboutevent.description') }}</p>
+      </section>
     </div>
   </div>
 </template>
@@ -27,4 +35,33 @@ export default class AboutEvent extends Vue {
 <style lang="scss">
 @import '../styles/variables';
 @import '~spectre.css/src/_layout';
+@import '~spectre.css/src/_media';
+
+.container-image {
+  display: block;
+  margin-right: 3%;
+  @media only screen and (max-device-width: 1200px) {
+    display: none;
+  }
+}
+.image-about-event {
+  height: 416px;
+  width: 416px;
+}
+.content {
+  flex-direction: row;
+  padding-left: 5%;
+  padding-right: 5%;
+}
+.text-content {
+  text-align: left;
+  .title,
+  .subtitle {
+    line-height: initial;
+    padding-bottom: 0;
+  }
+}
+.text {
+  line-height: 2rem;
+}
 </style>
---
+++ tests/unit/components/AboutEvent.spec.ts
@@ -0,0 +1,16 @@
+import { shallowMount } from '@vue/test-utils';
+import AboutEvent from '@/components/AboutEvent.vue';
+
+describe('About Event Test', ()=>{
+    const wrapper = shallowMount(AboutEvent, {
+        mocks: {
+          $t: () => {
+            return 'text any'
+          },
+        }
+      });
+    it('Image has atributtes src, alt, class', ()=>{
+        const image = wrapper.find('img[src alt class]');
+        expect(image.exists()).toBe(true)
+    });
+});
