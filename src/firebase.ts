32ff70bf8f1d6be83cbe7811a3d0b04c11a9ced3
Sinuhe Jaime <sierisimo@gmail.com>
2020-08-31 20:44:26 -0500

    Fixing variable names in firebase

--- src/firebase.ts
+++ src/firebase.ts
@@ -3,11 +3,13 @@ import 'firebase/firestore';
 
 const firebaseApp = firebase.initializeApp({
   apiKey: process.env.VUE_APP_FIREBASE_API,
+  appId: process.env.VUE_APP_FIREBASE_APP,
   authDomain: process.env.VUE_APP_FIREBASE_AUTH_DOMAIN,
   databaseURL: process.env.VUE_APP_FIREBASE_DB_URL,
   projectId: process.env.VUE_APP_FIREBASE_PROJECT,
   storageBucket: process.env.VUE_APP_FIREBASE_STORAGE,
-  messagingSenderId: process.env.VUE_APP_FIREBASE_MESSAGING_SENDER_ID
+  messagingSenderId: process.env.VUE_APP_FIREBASE_MESSAGING_SENDER,
+  measurementId: process.env.VUE_APP_FIREBASE_MEASUREMENT
 });
 
 const db = firebaseApp.firestore();
