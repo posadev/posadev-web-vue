eada4f894a2fe4af73f2452a92ade3b274ed2452
Yaidi <yaidi.garcia.lopez@gmail.com>
2020-08-31 22:33:15 -0500

    Trouble with testing

--- tests/unit/components/Banner.spec.ts
+++ tests/unit/components/Banner.spec.ts
@@ -1,24 +1,39 @@
+import * as firebase from 'firebase';
 import { mount } from '@vue/test-utils';
 import Banner from '@/components/Banner.vue';
 
 describe('testing Banner Component', () => {
-  const wrapper = mount(Banner, {
-    mocks: {
-      $t: () => {
-        return {
-          banner: {
-            subtitle: 'El evento de JAVA más importante de México'
-          }
-        };
-      }
-    }
-  });
+  beforeEach(() => {
+  jest.mock('@/service/fetchImageURL' );
+});
   it('paragraph text has the text correct', () => {
+    const wrapper = mount(Banner, {
+      mocks: {
+        $t: () => {
+          return {
+            banner: {
+              subtitle: 'El evento de JAVA más importante de México'
+            }
+          };
+        },
+      }
+    });
     const paragraph = wrapper.find('p[class="text"]');
     expect(paragraph.exists()).toBe(true);
   });
 
   it('should have at least two images, one for logo and one for background', () => {
+    const wrapper = mount(Banner, {
+      mocks: {
+        $t: () => {
+          return {
+            banner: {
+              subtitle: 'El evento de JAVA más importante de México'
+            }
+          };
+        },
+      }
+    });
     const images = wrapper.findAll('img');
     expect(images.length).toBeGreaterThan(1);
   });
