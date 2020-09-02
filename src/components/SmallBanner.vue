5834c416edae6284fddf332a1bacc50bd3bf8d22
GeorginaPQ <giorginapqe@gmail.com>
2020-09-01 21:54:09 -0500

    Create smallBanner component and show image

---
+++ src/components/SmallBanner.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="smallBanner"></div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+
+@Component({})
+export default class SmallBanner extends Vue {}
+</script>
+
+<style lang="scss">
+@import '../styles/variables';
+.smallBanner {
+  background-repeat: no-repeat;
+  background-size: cover;
+  background-position: center;
+  background-image: url('../assets/banner.png');
+  width: 100%;
+  height: 273px;
+}
+</style>
