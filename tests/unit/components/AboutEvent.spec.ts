ae7e72be038c572b0f6b55be9cd894946b5c7bca
Yaidi <yaidi.garcia.lopez@gmail.com>
2020-09-02 15:52:05 -0500

    fixing lint test file

--- tests/unit/components/AboutEvent.spec.ts
+++ tests/unit/components/AboutEvent.spec.ts
@@ -12,21 +12,21 @@ describe('About Event Test', () => {
       }
     }
   });
-  it('Image about event exist', async() => {
+  it('Image about event exist', async () => {
     const image = wrapper.find('img');
 
     await flushPromises();
 
-    expect(image.attributes('alt')).toBe("about event");
+    expect(image.attributes('alt')).toBe('about event');
   });
-  it('section text-content exist', async()=>{
+  it('section text-content exist', async () => {
     const section = wrapper.find('section[class="text-content"]');
 
     await flushPromises();
 
     expect(section.exists()).toBe(true);
-  })
-  it('section text-content has title, subtitle and text', async()=>{
+  });
+  it('section text-content has title, subtitle and text', async () => {
     const section = wrapper.find('section[class="text-content"]');
     const title = section.find('.title');
     const subtitle = section.find('.subtitle');
@@ -37,5 +37,5 @@ describe('About Event Test', () => {
     expect(title.exists()).toBe(true);
     expect(subtitle.exists()).toBe(true);
     expect(text.exists()).toBe(true);
-  })
+  });
 });
