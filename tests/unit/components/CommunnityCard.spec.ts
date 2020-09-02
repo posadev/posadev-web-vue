34b2f9080102bc3890c05f5f0939319f1f7dd21e
26erika <develop.erika@gmail.com>
2020-09-02 18:43:25 -0500

    Added blank line in test

--- tests/unit/components/CommunnityCard.spec.ts
+++ tests/unit/components/CommunnityCard.spec.ts
@@ -2,6 +2,7 @@ import { shallowMount } from '@vue/test-utils';
 import CommunityMock from '@/mocks/communities.mock';
 import CommunityCard from '@/components/CommunityCard.vue';
 import CommunityDetail from '@/components/CommunityDetail.vue';
+
 describe('CommunityCard.vue', () => {
   it('it should render properly', () => {
     const mock = CommunityMock[0];
