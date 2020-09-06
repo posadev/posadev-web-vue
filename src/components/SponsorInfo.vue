d13e5bcbd9d3122b48cc0ea209cd6c5d2e332fdd
GeorginaPQ <giorginapqe@gmail.com>
2020-09-05 21:29:41 -0500

    Create SponsorInfo component that renders the landigImageUrl and the SponsorDetail component

---
+++ src/components/SponsorInfo.vue
@@ -0,0 +1,35 @@
+<template>
+  <div
+    v-bind:class="[
+      'sponsor-card-box',
+      { 'left-aligned': alignLeft, 'right-aligned': !alignLeft }
+    ]"
+  >
+    <div class="sponsor-card-image">
+      <img class="img-sponsor" :src="this.sponsor.landingImageUrl" />
+    </div>
+    <div>
+      <SponsorDetail class="sponsor-card-detail" :sponsor="this.sponsor" />
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator';
+import SponsorDetail from '@/components/SponsorDetail.vue';
+import Sponsor from '@/data/Sponsor.model';
+
+@Component({
+  components: {
+    SponsorDetail
+  }
+})
+export default class SponsorInfo extends Vue {
+  @Prop({ required: true })
+  private sponsor!: Sponsor;
+
+  @Prop()
+  private alignLeft!: boolean;
+}
+</script>
+<style lang="scss"></style>
