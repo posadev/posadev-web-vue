8d12ed4aeb870e30cbe076054a93d84d13de21cb
Sinuhe Jaime <sierisimo@gmail.com>
2020-09-09 18:34:42 -0500

    Adding limit option for fetching data

--- src/service/FirebaseCollectionService.ts
+++ src/service/FirebaseCollectionService.ts
@@ -4,6 +4,8 @@ import FirestoreDataConverter = firebase.firestore.FirestoreDataConverter;
 import DocumentData = firebase.firestore.DocumentData;
 import QueryDocumentSnapshot = firebase.firestore.QueryDocumentSnapshot;
 import QuerySnapshot = firebase.firestore.QuerySnapshot;
+import CollectionReference = firebase.firestore.CollectionReference;
+import Query = firebase.firestore.Query;
 
 export type DocumentMapper<R> = (data: DocumentData) => R;
 
@@ -11,7 +13,6 @@ export abstract class FirebaseCollectionService<T> {
   abstract collectionName: string;
 
   abstract mapper: DocumentMapper<T>;
-
   converter: FirestoreDataConverter<T> = {
     toFirestore: (modelObject: T) => modelObject,
     fromFirestore: (snapshot: QueryDocumentSnapshot<T>) => {
@@ -20,12 +21,22 @@ export abstract class FirebaseCollectionService<T> {
   };
 
   async findAll(): Promise<T[]> {
-    return db
-      .collection(this.collectionName)
-      .withConverter(this.converter)
-      .get()
-      .then((document: QuerySnapshot<T>) => {
-        return document.docs.map((doc: QueryDocumentSnapshot<T>) => doc.data());
-      });
+    return this.createCollectionPromise(this.createCollectionRef());
+  }
+
+  async find(limit: number): Promise<T[]> {
+    const query = this.createCollectionRef().limit(limit);
+
+    return this.createCollectionPromise(query);
+  }
+
+  private createCollectionRef(): CollectionReference<T> {
+    return db.collection(this.collectionName).withConverter(this.converter);
+  }
+
+  private createCollectionPromise(query: Query<T>): Promise<T[]> {
+    return query.get().then((document: QuerySnapshot<T>) => {
+      return document.docs.map((doc: QueryDocumentSnapshot<T>) => doc.data());
+    });
   }
 }
