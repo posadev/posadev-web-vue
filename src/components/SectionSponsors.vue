d79140e58a6aa94b6188d3c550066b9ed3836d0c
GeorginaPQ <giorginapqe@gmail.com>
2020-09-14 15:09:10 -0500

    :alembic: Update UI for button

--- src/components/SectionSponsors.vue
+++ src/components/SectionSponsors.vue
@@ -1,5 +1,13 @@
 <template>
   <SectionHero class="section-home section-white" :title="sponsorsTexts">
+    <template v-slot:hero-header>
+      <AccentActionButton
+        id="btn-cfs"
+        class="section-button"
+        :info="sponsorsInfo"
+        v-on:button-action="onCFSClick"
+      />
+    </template>
     <template v-slot:hero-body>
       <SponsorList />
     </template>
@@ -9,13 +17,16 @@
 <script lang="ts">
 import { Component, Mixins } from 'vue-property-decorator';
 import TitleTexts from '@/data/TitleTexts.model';
+import ButtonInfo from '@/data/ButtonInfo.model';
 import SectionHero from '@/components/SectionHero.vue';
+import AccentActionButton from '@/components/AccentActionButton.vue';
 import SponsorList from '@/components/SponsorList.vue';
 import BannerLogoContainer from '@/di/BannerLogoContainer';
 
 @Component({
   components: {
     SectionHero,
+    AccentActionButton,
     SponsorList
   }
 })
@@ -26,7 +37,22 @@ export default class SectionSponsors extends Mixins(BannerLogoContainer) {
       this.$t('home.sponsors.subtitle')
     );
   }
+
+  private get sponsorsInfo(): ButtonInfo {
+    return new ButtonInfo(this.$t('home.sponsors.topButton'));
+  }
+
+  private onCFSClick() {
+    window.open(
+      'https://docs.google.com/document/d/1zWYvQ3RUKzu1ohTfcbYYdzQZd-0_YhgStFIWUEGFrRY/edit?usp=sharing'
+    );
+  }
 }
 </script>
 
-<style lang="scss"></style>
+<style lang="scss">
+
+.section-button {
+  display: none;
+}
+</style>
