69b3bf45594b043f4aec6aefb9ce94d89786552d
26erika <develop.erika@gmail.com>
2020-09-15 17:48:41 -0500

    Added path for communities

--- src/data/BarItem.type.ts
+++ src/data/BarItem.type.ts
@@ -6,5 +6,6 @@ export type BarItem = {
 export const navigationItems: BarItem[] = [
   { path: '/speakers', textRef: 'header.speakers' },
   { path: '/schedule', textRef: 'header.schedule' },
-  { path: '/sponsors', textRef: 'header.sponsors' }
+  { path: '/sponsors', textRef: 'header.sponsors' },
+  { path: '/communities', textRef: 'header.communities' }
 ];
