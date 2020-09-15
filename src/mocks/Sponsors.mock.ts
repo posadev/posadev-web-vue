7db40e61bf7b2bbc921ce3b899f570ab02b683f0
Sinuhe Jaime <sierisimo@gmail.com>
2020-09-15 16:49:35 -0500

    Fixing attribute at sponsors

--- src/mocks/Sponsors.mock.ts
+++ src/mocks/Sponsors.mock.ts
@@ -1,13 +1,12 @@
 import Sponsor from '@/data/Sponsor.model';
 
 const sponsors: Sponsor[] = [
-  new Sponsor('Soy Sier', 'gold', 'SierCorp', new URL('https://via.placeholder.com/196x84'), new URL('https://www.sierisimo.com/'),
+  new Sponsor('Soy Sier', 'SierCorp', new URL('https://via.placeholder.com/196x84'), new URL('https://www.sierisimo.com/'),
     {
       twitter: new URL('https://twitter.com/sierisimo')
     }, new URL('https://lanotapositiva.com/wp-content/uploads/2020/07/personas.jpg')),
   new Sponsor(
     'Soy IBM',
-    '',
     'IBM',
     new URL('https://via.placeholder.com/196x84'),
     new URL('https://www.ibm.com'),
