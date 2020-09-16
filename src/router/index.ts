3113ebef40a8ebd5095398bcb23231dd3b9457e3
26erika <develop.erika@gmail.com>
2020-09-16 13:42:13 -0500

    Fixed Scroll to the top in views

--- src/router/index.ts
+++ src/router/index.ts
@@ -41,6 +41,13 @@ const routes: Array<RouteConfig> = [
 const router = new VueRouter({
   mode: 'history',
   base: process.env.BASE_URL,
+  scrollBehavior(to, from, savedPosition) {
+    if (savedPosition) {
+      return savedPosition;
+    } else {
+      return { x: 0, y: 0 };
+    }
+  },
   routes
 });
 
