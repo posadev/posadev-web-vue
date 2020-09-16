5d1a804f6ae7c8c531612700882e7674b3ef9029
Sinuhe Jaime <sierisimo@gmail.com>
2020-09-16 03:56:37 -0500

    Simple message to render

--- src/views/ScheduleView.vue
+++ src/views/ScheduleView.vue
@@ -1,16 +1,12 @@
 <template>
-
+  <h1>In progress</h1>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
 
 @Component
-export default class ScheduleView extends Vue {
-
-}
+export default class ScheduleView extends Vue {}
 </script>
 
-<style lang="scss">
-
-</style>
+<style lang="scss"></style>
