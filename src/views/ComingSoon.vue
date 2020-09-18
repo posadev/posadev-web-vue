449a671fe7065eb2a899781c9cf56461b1d1e194
GeorginaPQ <giorginapqe@gmail.com>
2020-09-17 21:59:38 -0500

    :fire: rename files

--- src/views/ComingSoon.vue
+++ src/views/ComingSoon.vue
@@ -4,10 +4,12 @@
 <style lang="scss">
 @import '../styles/variables';
 .coming-soon {
-  background-repeat: no-repeat;
-  background-size: contain;
-  background-position: center;
-  background-image: url('../assets/comingSoon.png');
+  background: {
+    repeat: no-repeat;
+    size: contain;
+    position: center;
+    image: url('../assets/comingSoon.png');
+  }
   height: 80vh;
 }
 </style>
