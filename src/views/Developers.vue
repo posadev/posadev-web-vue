d77d62158343c4f6278829900736c20cdbd30310
26erika <develop.erika@gmail.com>
2020-09-25 22:11:07 -0500

    Add info card for developers

--- src/views/Developers.vue
+++ src/views/Developers.vue
@@ -1,48 +1,99 @@
 <template>
   <div class="developers">
     <ViewHeader :header-texts="headerText" />
-    <div class="dev-section">
-      <div class="image-logo">
-        <div class="dev-info">
-          <h4
-            class="fer"
-            @click="
-              goDevProfile(
-                'https://www.linkedin.com/in/fernando-gonzalez-camacho-4207aa56/'
-              )
-            "
-          >
-            FERNANDO GONZALEZ
-          </h4>
-          <h4 class="si" @click="goDevProfile('https://www.sierisimo.com/')">
-            SINUHE JAIME
-          </h4>
-          <h4
-            class="geo"
-            @click="goDevProfile('https://www.linkedin.com/in/georginapq/')"
-          >
-            GEORGINA PEREZ
-          </h4>
-          <h4
-            class="eri"
-            @click="
-              goDevProfile('https://www.linkedin.com/in/erika-sanchez-murguia/')
-            "
-          >
-            ERIKA SANCHEZ
-          </h4>
-          <h4
-            class="yai"
-            @click="goDevProfile('https://www.linkedin.com/in/yaidi-garcia/')"
-          >
-            YAIDI GARCIA
-          </h4>
-          <h4
-            class="eug"
-            @click="goDevProfile('https://www.linkedin.com/in/eugenianajar/')"
-          >
-            EUGENIA NAJAR
-          </h4>
+    <div class="image-logo">
+      <div class="dev-info">
+        <div class="dev-container">
+          <img
+            src="https://media-exp1.licdn.com/dms/image/C4E03AQEA9rNSeGGcSg/profile-displayphoto-shrink_400_400/0?e=1606348800&v=beta&t=rD6fypwUhtQWAEBqcOwX70owwL4lB-6juA76oy-SYbI"
+            alt="Fernando"
+          />
+          <div class="dev-aside">
+            <h2>FERNANDO GONZALEZ</h2>
+            <img
+              src="../assets/linkedin.png"
+              alt="linkedin"
+              @click="
+                goDevProfile(
+                  'https://www.linkedin.com/in/fernando-gonzalez-camacho-4207aa56/'
+                )
+              "
+            />
+          </div>
+        </div>
+        <div class="dev-container">
+          <img
+            src="https://media-exp1.licdn.com/dms/image/C4E03AQEdnDOL1UTg6g/profile-displayphoto-shrink_400_400/0?e=1606348800&v=beta&t=ntcQoVaksvdKhu3RA6UaGeqr1R5DO8Ipql4tnodt6bE"
+            alt="Sinuhe"
+          />
+          <div class="dev-aside">
+            <h2>SINUHE JAIME</h2>
+            <img
+              src="../assets/linkedin.png"
+              alt="linkedin"
+              @click="goDevProfile('https://www.linkedin.com/in/sierisimo/')"
+            />
+          </div>
+        </div>
+        <div class="dev-container">
+          <img
+            src="https://media-exp1.licdn.com/dms/image/C4D03AQFNuCtWdPJ9Aw/profile-displayphoto-shrink_400_400/0?e=1606348800&v=beta&t=45yrJydSFc4S0mimllWM1k7XrdHqFQ5RGAj9Kx7Znto"
+            alt="Georgina"
+          />
+          <div class="dev-aside">
+            <h2>GEORGINA PEREZ</h2>
+            <img
+              src="../assets/linkedin.png"
+              alt="linkedin"
+              @click="goDevProfile('https://www.linkedin.com/in/georginapq/')"
+            />
+          </div>
+        </div>
+        <div class="dev-container">
+          <img
+            src="https://media-exp1.licdn.com/dms/image/C4E03AQE0bMI96eYlAg/profile-displayphoto-shrink_400_400/0?e=1606348800&v=beta&t=9QsD67jLBxQyLgDbx9YzRmxbYvDDLeE-rdtLCvc0nQA"
+            alt="Erika"
+          />
+          <div class="dev-aside">
+            <h2>ERIKA SANCHEZ</h2>
+            <img
+              src="../assets/linkedin.png"
+              alt="linkedin"
+              @click="
+                goDevProfile(
+                  'https://www.linkedin.com/in/erika-sanchez-murguia/'
+                )
+              "
+            />
+          </div>
+        </div>
+        <div class="dev-container">
+          <img
+            src="https://media-exp1.licdn.com/dms/image/C4E03AQFnMb6xqG6Vyw/profile-displayphoto-shrink_400_400/0?e=1606348800&v=beta&t=yGdBnVsD7scAEtCasOHuLbJn-o0WpFbxy5uRHTFGmGo"
+            alt="Yaidi"
+          />
+          <div class="dev-aside">
+            <h2>YAIDI GARCIA</h2>
+            <img
+              src="../assets/linkedin.png"
+              alt="linkedin"
+              @click="goDevProfile('https://www.linkedin.com/in/yaidi-garcia/')"
+            />
+          </div>
+        </div>
+        <div class="dev-container">
+          <img
+            src="https://media-exp1.licdn.com/dms/image/C5603AQF5BaFqMsO_SQ/profile-displayphoto-shrink_400_400/0?e=1606348800&v=beta&t=Yb17LGRq7ek9wZN_zwSnhcO611QHvw5AcsL8QJwvA1g"
+            alt="Eugenia"
+          />
+          <div class="dev-aside">
+            <h2>EUGENIA NAJAR</h2>
+            <img
+              src="../assets/linkedin.png"
+              alt="linkedin"
+              @click="goDevProfile('https://www.linkedin.com/in/eugenianajar/')"
+            />
+          </div>
         </div>
       </div>
     </div>
@@ -91,21 +142,51 @@ export default class SponsorView extends Vue {
 }
 
 .dev-info {
-  position: absolute;
-  top: 55%;
-  left: 50%;
-  transform: translate(-50%, -50%);
-  color: black;
-  cursor: pointer;
-  @include media-screen-max-width(1024px) {
-    font-size: 12px;
-    top: 55%;
+  columns: 200px 3;
+  width: 90%;
+  height: 100%;
+  margin: 0 auto;
+  padding-top: 2rem;
+  @include media-screen-max-width(1440px) {
+    width: 90%;
   }
   @include media-screen-max-width(768px) {
-    top: 55%;
+    columns: 200px 2;
   }
-  @include media-screen-max-width(768px) {
-    font-size: 12px;
+}
+
+.dev-container {
+  display: flex;
+  margin-bottom: 50px;
+  width: 100%;
+  height: 140px;
+  text-align: left;
+  background: $backgorund-banner-color;
+  color: $light-color;
+  border: 1px solid $dark-color;
+  border-radius: 5px;
+  box-shadow: 0 5px 10px $dark-color;
+  img {
+    width: 140px;
+    border-radius: 5px;
+  }
+}
+
+.dev-aside {
+  vertical-align: top;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  height: 100%;
+  width: 200px;
+  margin-left: 10px;
+  h2 {
+    margin-top: 10px;
+  }
+  img {
+    width: 40px;
+    margin-bottom: 5px;
+    cursor: pointer;
   }
 }
 </style>
